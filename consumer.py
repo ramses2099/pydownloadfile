8aa32451756468758d7f95d5969ba0a6eeffeda6
ramses2099 <ramses2099@gmail.com>
2024-11-01 15:13:36 -0400

    latest changes

--- consumer.py
+++ consumer.py
@@ -21,16 +21,28 @@ topic = "topicnews"
 brokers = "localhost:9092"
 
 def main() -> None:
-    # Create the Kafka consumer
-    consumer = KafkaConsumer(topic, 
-                             auto_offset_reset='earliest',
-                             enable_auto_commit=False,
-                             value_deserializer=lambda m: json.loads(m.decode('utf-8')))
     
-    # Continuously poll for new messages
-    for message in consumer:
-        print("topic: %s" % message.topic)
-        print(f"m {message.value}")
+        # Create the Kafka consumer
+    consumer = KafkaConsumer(topic, 
+                                auto_offset_reset='earliest',
+                                enable_auto_commit=False,
+                                value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+    try:
+        print("Start Consuming for messages...")
+        
+        with open("news.csv",'w', encoding="utf-8") as f:
+            columns ="source,publishedAt,author,title,description,content\n"            
+            f.write(columns)
+            # Continuously poll for new messages
+            for message in consumer:
+                data = message.value            
+                line = f"{data['source']},{data['publishedAt']},{data['author']},{data['title']},{data['description']},{data['content']}\n"
+                f.write(line)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        consumer.close(autocommit=False)
+        print("Finished Consuming for messages...")
 
 
 if __name__ == '__main__':
