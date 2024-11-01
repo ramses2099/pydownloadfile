3c2cb4dcbd5d396c96ec7d626b74ae103d83ef9b
ramses2099 <ramses2099@gmail.com>
2024-11-01 14:24:24 -0400

    latest changes

--- consumer.py
+++ consumer.py
@@ -2,11 +2,7 @@ import os
 import sys
 import six
 
-from kafka import KafkaConsumer
-from dotenv import load_dotenv
-
-if sys.version_info >= (3, 12, 0):
-    sys.modules['kafka.vendor.six.moves'] = six.moves
+from confluent_kafka import Consumer
 
 
 ################################################################
@@ -17,16 +13,39 @@ if sys.version_info >= (3, 12, 0):
 ################################################################
 
 
-topic="topicnews"
-brokers = "localhost:9092"
-
 def main() -> None:
-    consumer = KafkaConsumer(topic)
-    print("Starting consumer...")
-    if consumer:
-        print("Consumer is running...")
-        for message in consumer:            
-            print(f'{message}')
+    config = {
+        # User-specific properties that you must set
+        'bootstrap.servers': 'localhost:9092',
+    }
+
+    # Create Consumer instance
+    consumer = Consumer(config)
+
+    # Subscribe to topic
+    topic = "topicnews"
+    consumer.subscribe([topic])
+
+    # Poll for new messages from Kafka and print them.
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                # Initial message consumption may take up to
+                # `session.timeout.ms` for the consumer group to
+                # rebalance and start consuming
+                print("Waiting...")
+            elif msg.error():
+                print("ERROR: %s".format(msg.error()))
+            else:
+                # Extract the (optional) key and value, and print.
+                print("Consumed event from topic {topic}: key = {key:12} value = {value:12}".format(
+                    topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Leave group and commit final offsets
+        consumer.close()
 
 if __name__ == '__main__':
     main()
